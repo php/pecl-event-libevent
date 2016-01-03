657ad92f5fc850c87c4704c1a8678c989562a176
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-01-03 12:06:36 +0100

    add php 7.0.2 tests

--- scripts/gen_travis_yml.php
+++ scripts/gen_travis_yml.php
@@ -15,7 +15,7 @@ env:
 
 $gen = include __DIR__ . "/../travis/pecl/gen-matrix.php";
 $env = $gen([
-	"PHP" => ["master"],
+	"PHP" => ["master", "7.0.2"],
 	"enable_debug",
 	"enable_maintainer_zts",
 ]);
